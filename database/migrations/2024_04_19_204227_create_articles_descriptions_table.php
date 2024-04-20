b9ba99782db3d645082c436ba35f46db2df7ea7f
Mohamed13796 <mohamedyasser13796@gmail.com>
2024-04-20 22:51:53 +0200

    updated successfully

---
+++ database/migrations/2024_04_19_204227_create_articles_descriptions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles_descriptions', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('article_id')->unsigned()->index();
+            $table->longText('description')->nullable();
+            $table->string('card_image')->nullable();
+            $table->foreign('article_id')->references('id')->on('articles')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles_descriptions');
+    }
+};
--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('admin.index');
-});
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('/',CategoryController::class);
